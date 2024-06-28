7c9170ba2978a9aab7ddeb5f3dcc41acaa163bd4
Andrey Seluykov <trezortop92@gmail.com>
2024-06-28 15:04:41 +0500

    implemented game rendering and ticks

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-mod utils;
-
+use std::fmt::{Display, Formatter};
 use wasm_bindgen::prelude::*;
+
+mod utils;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,11 +10,132 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+// each cell is represented as a single byte
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(format!("Hello, {name}", ).as_str());
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+impl Display for Universe {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        for row in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in row {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+
+                write!(f, "{}", symbol)?;
+            }
+
+            writeln!(f)?;
+        }
+
+        Ok(())
+    }
+}
+
+impl Default for Universe {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+/// Public methods, exported to JavaScript.
+#[wasm_bindgen]
+impl Universe {
+    pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..width * height)
+            .map(|x| {
+                if x % 2 == 0 || x % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+
+    pub fn tick(&mut self) {
+        let mut next_generation = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let index = self.get_index(row, col);
+                let cell = self.cells[index];
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                let next_cell_state = match (cell, live_neighbors) {
+                    // Rule 1: Any live cell with fewer than two live neighbours
+                    // dies, as if caused by underpopulation.
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    // Rule 2: Any live cell with two or three live neighbours
+                    // lives on to the next generation.
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    // Rule 3: Any live cell with more than three live
+                    // neighbours dies, as if by overpopulation.
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    // Rule 4: Any dead cell with exactly three live neighbours
+                    // becomes a live cell, as if by reproduction.
+                    (Cell::Dead, 3) => Cell::Alive,
+                    // All other cells remain in the same state.
+                    (otherwise, _) => otherwise,
+                };
+
+                next_generation[index] = next_cell_state;
+            }
+        }
+
+        self.cells = next_generation;
+    }
+
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+
+        for delta_row in [-1, 0, 1].iter() {
+            for delta_col in [-1, 0, 1].iter() {
+                if *delta_row == 0 && *delta_col == 0 {
+                    continue;
+                }
+
+                let row_index = (row as i32 + delta_row) as u32;
+                let col_index = (column as i32 + delta_col) as u32;
+
+                // For example, if self.height is 10 and row_index is -1 (indicating the cell above the first row),
+                // (-1 + 10) % 10 equals 9, which is the index of the last row.
+                // So, the cell above the first row is considered to be the last row
+                let neighbor_row = (row_index + self.height) % self.height;
+                let neighbor_col = (col_index + self.width) % self.width;
+                let index = self.get_index(neighbor_row, neighbor_col);
+                // The as u8 cast converts the Cell enum value to an integer.
+                // If the cell is Dead, it will be converted to 0, and if it's Alive, it will be converted to 1.
+                count += self.cells[index] as u8;
+            }
+        }
+
+        count
+    }
 }
--- web-app/src/main.ts
+++ web-app/src/main.ts
@@ -1,3 +1,19 @@
-import * as wasm from "wasm-pkg";
+import {Universe} from "wasm-pkg"
 
-wasm.greet("WebAssembly from Vite!")
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+if (!pre) {
+    throw new Error("Failed to find root element");
+}
+
+const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+
+    setTimeout(() => {
+        requestAnimationFrame(renderLoop);
+    }, 50);
+};
+
+requestAnimationFrame(renderLoop);
